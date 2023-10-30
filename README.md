214648cb6b3bdb9921fd5e6b32e54c1a5a98771d
Tony Bhimani <tonybhimani@users.noreply.github.com>
2023-10-30 13:47:45 -0700

    Initial commit

---
+++ README.md
@@ -0,0 +1,34 @@
+# Potato Talker App
+How to describe this app... It was a "hey, I want you to add a talking avatar to your sound recorder" but then I decided to take that idea and make it an app. Basically, it's a an app that has an animated GIF, text field for input, and tele-types / prints the same message like a parrot, meanwhile the GIF plays with the Potato's mouth flapping as if he's saying it.
+
+The package uses libgif-js (SuperGif) for playing/pausing the animated GIF, and basic Javascript for the rest, bootstrap to doll it up, all baked in Cordova that I built for Android. You could build it for IOS too if you want.
+
+**Resources**
+libgif-js
+https://github.com/buzzfeed/libgif-js
+
+Potato GIF
+https://tenor.com/view/potato-talk-blob-face-speak-gif-22490759
+
+So how to get it all running? First you need Node, Cordova, etc then follow this
+```
+git clone https://github.com/tonybhimani/potato-talker-app.git
+cd potato-talker-app
+npm install
+cordova platform add browser
+cordova platform add android
+
+```
+
+then you can run it in the browser or build it for Android
+
+```
+cordova run browser
+cordova run android
+```
+
+Note: I modded the libgif-js script to include an optional config parameter called `frame_delay` so that the default GIF animation speed can be overridden.
+
+TODOs: Fix the text wrap then add in some text-to-speech capability. If I wanted to go hardcore, give the Potato Talker some inteeligence with ChatGPT.
+
+Happy coding!
---
+++ www/js/index.js
@@ -0,0 +1,57 @@
+document.addEventListener('deviceready', onDeviceReady, false);
+
+function onDeviceReady() {
+    // libgif.js to handle the Potato Talker gif
+    var sup1 = new SuperGif({ gif: document.getElementById("potato"), auto_play: false, frame_delay: 10 } );
+    sup1.load(() => {
+        // do something after load
+    });
+
+    // collection of timeouts for cleanup
+    var timeouts = [];
+
+    // handle to the "Say It" button
+    var sayit = document.getElementById("sayit");
+
+    // click event for the "Say It" button
+    sayit.addEventListener("click", (e) => {
+        var saywhat = document.getElementById("saywhat");
+        var whatissaid = document.getElementById("whatissaid");
+        // make sure you saniatize your input, unlike me
+        var saywhat_str = saywhat.value.trim();
+        // process the input if there is some
+        if (saywhat_str.length > 0) {
+            // start the Potato Talker
+            sup1.play();
+            whatissaid.innerHTML = "";
+            sayit.disabled = true;
+            var idx = 0;
+            // clear any existing timeouts for removing potato jabber
+            if (timeouts.length > 0) {
+                for (var i = 0; i < timeouts.length; i++) {
+                    clearTimeout(timeouts[i]);
+                }
+                timeouts.length = 0;
+            }
+            // Potato Talker speaks character by character
+            var t1 = setInterval(() => {
+                whatissaid.innerHTML += saywhat_str.charAt(idx);
+                if (++idx == saywhat_str.length) { 
+                    clearInterval(t1);
+                    sup1.pause();
+                    sup1.move_to(0);
+                    sayit.disabled = false;
+                    // remove the potato jabber after 3 seconds
+                    timeouts.push(
+                        setTimeout(() => { 
+                            whatissaid.innerHTML = "";
+                        }, 3000)
+                    );
+                }
+            }, 50);
+        } else {
+            // it'd be better if the potato said this
+            alert("Type something to say!");
+        }
+    });
+}
