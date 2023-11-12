5aececb046c6ac1723c3a7295ba04bb508bd8644
Tony Bhimani <tonybhimani@users.noreply.github.com>
2023-11-12 11:08:25 -0800

    TTS Settings
    Added TTS settings for speech identifier, rate, and pitch for user control. Settings are persistent.

--- README.md
+++ README.md
@@ -1,7 +1,7 @@
 # Potato Talker App
 ![Poatato Talker Preview](https://github.com/tonybhimani/potato-talker-app/blob/7e1e2911f5b1813e5043b5df02dc7d3d40d9864a/preview.gif?raw=true)
 
-How to describe this app... It was a "hey, I want you to add a talking avatar to your sound recorder" but then I decided to take that idea and make it an app. Basically, it's a an app that has an animated GIF, text field for input, and tele-types / prints the same message like a parrot, meanwhile the GIF plays with the Potato's mouth flapping as if he's saying it.
+How to describe this app... It was a "hey, I want you to add a talking avatar to your sound recorder" but then I decided to take that idea and make it an app. Basically, it's an app that has an animated GIF, text field for input, and tele-types / prints the same message like a parrot, meanwhile the GIF plays with the Potato's mouth flapping as if he's saying it.
 
 The package uses libgif-js (SuperGif) for playing/pausing the animated GIF, and basic Javascript for the rest, bootstrap to doll it up, all baked in Cordova that I built for Android. You could build it for iOS too if you want.
 
@@ -18,6 +18,7 @@ So how to get it all running? First you need Node, Cordova, etc then follow this
 git clone https://github.com/tonybhimani/potato-talker-app.git
 cd potato-talker-app
 npm install
+cordova plugin add cordova-plugin-tts-advanced
 cordova platform add browser
 cordova platform add android
 ```
@@ -31,7 +32,19 @@ cordova run android
 
 ## Updates
 
-11/09/2023 - Added text-to-speech using the `cordova-plugin-tts-advanced` plugin.
+* 11/09/2023 - Added text-to-speech using the `cordova-plugin-tts-advanced` plugin.
+* 11/12/2023 - Added TTS settings so voice, rate, and pitch can be changed. Settings are persistent.
+
+## Branches
+
+I've decided to have different branches of this project for each version instead of overwriting previous changes. Also, if I put out releases they can be for each branch intead of a jumbled mess of choices. That being said, the branches are:
+
+* **main**: User text input, `cordova-plugin-tts-advanced` for speech processing, the potato will be a parrot to what is typed.
+* **speech-input**: User verbal input, `cordova-plugin-speech` for speech and voice input processing, the potato will be a parrot to what is spoken.
+* **chatbot-gpt**: User verbal input, `cordova-plugin-speech` for speech and voice input processing, the potato will attempt to generate responses from ChatGPT.
+* **chatbot-tf**:  User verbal input, `cordova-plugin-speech` for speech and voice input processing, the potato will attempt to generate responses from TensorFlow.
+
+That's a glimpse at my roadmap although nothing is set in stone.
 
 ## Conclusion
 
--- www/js/index.js
+++ www/js/index.js
@@ -1,7 +1,37 @@
-document.addEventListener('deviceready', onDeviceReady, false);
+// default app settings
+var settings = {
+    identifier: "",
+    rate: 2.0,
+    pitch: 0.02,
+};
 
+// get app settings from storage
+function getSettings() {
+    // these settings should tamper-tested before usage
+    settings.identifier = window.localStorage.getItem("identifier") || settings.identifier;
+    settings.rate = window.localStorage.getItem("rate") || settings.rate;
+    settings.pitch = window.localStorage.getItem("pitch") || settings.pitch;
+}
+
+// save app settings to storage
+function setSettings() {
+    // these settings should be tamper-tested before saving
+    window.localStorage.setItem("identifier", settings.identifier);
+    window.localStorage.setItem("rate", settings.rate);
+    window.localStorage.setItem("pitch", settings.pitch);
+}
+
+// add event listeners
+//  the resume and pause events technically aren't necessary since the
+//  settings are saved upon change, BUT they're here anyway
+document.addEventListener('deviceready', onDeviceReady, false);
+document.addEventListener('resume', getSettings, false);
+document.addEventListener('pause', setSettings, false);
+
+// where the magic happens...
 function onDeviceReady() {
     // libgif.js to handle the Potato Talker gif
+    //   TODO: dynamically update the frame_delay per the speech rate so the lip-syncing is more approximate
     var sup1 = new SuperGif({ gif: document.getElementById("potato"), auto_play: false, frame_delay: 10 } );
     sup1.load(() => {
         // do something after load
@@ -11,7 +41,70 @@ function onDeviceReady() {
     var timeouts = [];
 
     // handle to the "Say It" button
-    var sayit = document.getElementById("sayit");
+    const sayit = document.getElementById("sayit");
+
+    // handles to the voice identifiers, rate, and pitch
+    const tts_identifiers = document.getElementById("tts_identifiers");
+    const tts_rate = document.getElementById("tts_rate");
+    const tts_pitch = document.getElementById("tts_pitch");
+    const tts_rate_text = document.getElementById("tts_rate_text");
+    const tts_pitch_text = document.getElementById("tts_pitch_text");
+
+    // load saved settings
+    getSettings();
+
+    // get tts identifiers
+    TTS.getVoices()
+        .then(function(voices) {
+            for (i = 0; i < voices.length; i++) {
+                var opt = document.createElement("option");
+                opt.value = voices[i].identifier;
+                opt.innerHTML = voices[i].name + " (" + voices[i].language + ")";
+                tts_identifiers.appendChild(opt);
+            }
+            // set the identifier from settings
+            tts_identifiers.value = settings.identifier;
+        },
+        function(err) {
+            alert("TTS voices failed: " + err);
+        });
+
+    // set the rate and pitch control values
+    tts_rate_text.innerText = tts_rate.value = settings.rate;
+    tts_pitch_text.innerText = tts_pitch.value = settings.pitch;
+
+    // change event for Identifiers list
+    tts_identifiers.addEventListener("change", (e) => {
+        settings.identifier = tts_identifiers.value;
+        console.log("TTS Identifier: " + tts_identifiers.value);
+        setSettings();
+    });
+
+    // input event for Rate slider (real-time change)
+    tts_rate.addEventListener("input", (e) => {
+        settings.rate = tts_rate.value;
+        tts_rate_text.innerText = tts_rate.value;
+        console.log("TTS Rate: " + tts_rate.value);
+    });
+
+    // change event for Rate slide (Regis final answer)
+    tts_rate.addEventListener("change", (e) => {
+        // it's better to save here once the user has finished dragging
+        setSettings();
+    });
+
+    // input event for Pitch slider (real-time change)
+    tts_pitch.addEventListener("input", (e) => {
+        settings.pitch = tts_pitch.value;
+        tts_pitch_text.innerText = tts_pitch.value;
+        console.log("TTS Pitch: " + tts_pitch.value);
+    });
+
+    // change event for Pitch slide (Regis final answer)
+    tts_pitch.addEventListener("change", (e) => {
+        // it's better to save here once the user has finished dragging
+        setSettings();
+    });
 
     // click event for the "Say It" button
     sayit.addEventListener("click", (e) => {
@@ -34,11 +127,14 @@ function onDeviceReady() {
                 timeouts.length = 0;
             }
             // use TTS plugin for voice
+            //   TODO: there is different plugin I may use instead
+            //   since it has events for when speech starts and ends
+            //   controlling the animation start/stop would be better
             TTS.speak({
                 text: saywhat_str,
-                identifier: "", // TODO: deal with this later
-                rate: 2.0,
-                pitch: 0.2,
+                identifier: tts_identifiers.value,
+                rate: tts_rate.value,
+                pitch: tts_pitch.value,
                 cancel: true
             },
             function() {
