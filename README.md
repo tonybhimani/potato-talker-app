4804f9dec8b19615267ff4001b2dd1704a285b5c
Tony Bhimani <tonybhimani@users.noreply.github.com>
2023-11-17 22:48:20 -0800

    initial commit

--- README.md
+++ README.md
@@ -1,4 +1,7 @@
 # Potato Talker App
+
+## speech-input branch
+
 ![Poatato Talker Preview](https://github.com/tonybhimani/potato-talker-app/blob/7e1e2911f5b1813e5043b5df02dc7d3d40d9864a/preview.gif?raw=true)
 
 How to describe this app... It was a "hey, I want you to add a talking avatar to your sound recorder" but then I decided to take that idea and make it an app. Basically, it's an app that has an animated GIF, text field for input, and tele-types / prints the same message like a parrot, meanwhile the GIF plays with the Potato's mouth flapping as if he's saying it.
@@ -14,35 +17,33 @@ Potato GIF
 https://tenor.com/view/potato-talk-blob-face-speak-gif-22490759
 
 So how to get it all running? First you need Node, Cordova, etc then follow this
+
 ```
 git clone https://github.com/tonybhimani/potato-talker-app.git
 cd potato-talker-app
 npm install
-cordova plugin add cordova-plugin-tts-advanced
-cordova platform add browser
+cordova plugin add cordova-plugin-speech
 cordova platform add android
 ```
 
-then you can run it in the browser or build it for Android
+then you can build and run it on Android
 
 ```
-cordova run browser
 cordova run android
 ```
 
 ## Updates
 
-* 11/09/2023 - Added text-to-speech using the `cordova-plugin-tts-advanced` plugin.
-* 11/12/2023 - Added TTS settings so voice, rate, and pitch can be changed. Settings are persistent.
+- 11/17/2023 - First release of this branch.
 
 ## Branches
 
 I've decided to have different branches of this project for each version instead of overwriting previous changes. Also, if I put out releases they can be for each branch intead of a jumbled mess of choices. That being said, the branches are:
 
-* **main**: User text input, `cordova-plugin-tts-advanced` for speech processing, the potato will be a parrot to what is typed.
-* **speech-input**: User verbal input, `cordova-plugin-speech` for speech and voice input processing, the potato will be a parrot to what is spoken.
-* **chatbot-gpt**: User verbal input, `cordova-plugin-speech` for speech and voice input processing, the potato will attempt to generate responses from ChatGPT.
-* **chatbot-tf**:  User verbal input, `cordova-plugin-speech` for speech and voice input processing, the potato will attempt to generate responses from TensorFlow.
+- **main**: User text input, `cordova-plugin-tts-advanced` for speech processing, the potato will be a parrot to what is typed.
+- **speech-input**: User verbal input, `cordova-plugin-speech` for speech and voice input processing, the potato will be a parrot to what is spoken.
+- **chatbot-gpt**: User verbal input, `cordova-plugin-speech` for speech and voice input processing, the potato will attempt to generate responses from ChatGPT.
+- **chatbot-tf**: User verbal input, `cordova-plugin-speech` for speech and voice input processing, the potato will attempt to generate responses from TensorFlow.
 
 That's a glimpse at my roadmap although nothing is set in stone.
 
--- www/js/index.js
+++ www/js/index.js
@@ -1,167 +1,212 @@
 // default app settings
 var settings = {
-    identifier: "",
-    rate: 2.0,
-    pitch: 0.02,
+  identifier: "",
+  rate: 2.0,
+  pitch: 0.02,
 };
 
 // get app settings from storage
 function getSettings() {
-    // these settings should tamper-tested before usage
-    settings.identifier = window.localStorage.getItem("identifier") || settings.identifier;
-    settings.rate = window.localStorage.getItem("rate") || settings.rate;
-    settings.pitch = window.localStorage.getItem("pitch") || settings.pitch;
+  // these settings should tamper-tested before usage
+  settings.identifier =
+    window.localStorage.getItem("identifier") || settings.identifier;
+  settings.rate = window.localStorage.getItem("rate") || settings.rate;
+  settings.pitch = window.localStorage.getItem("pitch") || settings.pitch;
 }
 
 // save app settings to storage
 function setSettings() {
-    // these settings should be tamper-tested before saving
-    window.localStorage.setItem("identifier", settings.identifier);
-    window.localStorage.setItem("rate", settings.rate);
-    window.localStorage.setItem("pitch", settings.pitch);
+  // these settings should be tamper-tested before saving
+  window.localStorage.setItem("identifier", settings.identifier);
+  window.localStorage.setItem("rate", settings.rate);
+  window.localStorage.setItem("pitch", settings.pitch);
 }
 
 // add event listeners
 //  the resume and pause events technically aren't necessary since the
 //  settings are saved upon change, BUT they're here anyway
-document.addEventListener('deviceready', onDeviceReady, false);
-document.addEventListener('resume', getSettings, false);
-document.addEventListener('pause', setSettings, false);
+document.addEventListener("deviceready", onDeviceReady, false);
+document.addEventListener("resume", getSettings, false);
+document.addEventListener("pause", setSettings, false);
 
 // where the magic happens...
 function onDeviceReady() {
-    // libgif.js to handle the Potato Talker gif
-    //   TODO: dynamically update the frame_delay per the speech rate so the lip-syncing is more approximate
-    var sup1 = new SuperGif({ gif: document.getElementById("potato"), auto_play: false, frame_delay: 10 } );
-    sup1.load(() => {
-        // do something after load
-    });
+  // libgif.js to handle the Potato Talker gif
+  //   TODO: dynamically update the frame_delay per the speech rate so the lip-syncing is more approximate
+  var sup1 = new SuperGif({
+    gif: document.getElementById("potato"),
+    auto_play: false,
+    frame_delay: 10,
+  });
+  sup1.load(() => {
+    // do something after load
+  });
 
-    // collection of timeouts for cleanup
-    var timeouts = [];
+  // collection of timeouts for cleanup
+  var timeouts = [];
 
-    // handle to the "Say It" button
-    const sayit = document.getElementById("sayit");
+  // handle to the "Repeat This" button
+  const repeatthis = document.getElementById("repeatthis");
 
-    // handles to the voice identifiers, rate, and pitch
-    const tts_identifiers = document.getElementById("tts_identifiers");
-    const tts_rate = document.getElementById("tts_rate");
-    const tts_pitch = document.getElementById("tts_pitch");
-    const tts_rate_text = document.getElementById("tts_rate_text");
-    const tts_pitch_text = document.getElementById("tts_pitch_text");
+  // handle to the spoken text div
+  const whatissaid = document.getElementById("whatissaid");
 
-    // load saved settings
-    getSettings();
+  // handles to the voice identifiers, rate, and pitch
+  const tts_identifiers = document.getElementById("tts_identifiers");
+  const tts_rate = document.getElementById("tts_rate");
+  const tts_pitch = document.getElementById("tts_pitch");
+  const tts_rate_text = document.getElementById("tts_rate_text");
+  const tts_pitch_text = document.getElementById("tts_pitch_text");
 
-    // get tts identifiers
-    TTS.getVoices()
-        .then(function(voices) {
-            for (i = 0; i < voices.length; i++) {
-                var opt = document.createElement("option");
-                opt.value = voices[i].identifier;
-                opt.innerHTML = voices[i].name + " (" + voices[i].language + ")";
-                tts_identifiers.appendChild(opt);
-            }
-            // set the identifier from settings
-            tts_identifiers.value = settings.identifier;
-        },
-        function(err) {
-            alert("TTS voices failed: " + err);
-        });
+  // load saved settings
+  getSettings();
 
-    // set the rate and pitch control values
-    tts_rate_text.innerText = tts_rate.value = settings.rate;
-    tts_pitch_text.innerText = tts_pitch.value = settings.pitch;
+  // get tts identifiers
+  Speech.getSupportedVoices(
+    function (voices) {
+      for (i = 0; i < voices.length; i++) {
+        var opt = document.createElement("option");
+        opt.value = voices[i].name;
+        opt.innerHTML = voices[i].name + " (" + voices[i].language + ")";
+        tts_identifiers.appendChild(opt);
+      }
+      // set the identifier from settings
+      tts_identifiers.value = settings.identifier;
+    },
+    function (err) {
+      alert("Speech.getSupportedVoices Failed: " + err);
+    }
+  );
 
-    // change event for Identifiers list
-    tts_identifiers.addEventListener("change", (e) => {
-        settings.identifier = tts_identifiers.value;
-        console.log("TTS Identifier: " + tts_identifiers.value);
-        setSettings();
-    });
+  // set the rate and pitch control values
+  tts_rate_text.innerText = tts_rate.value = settings.rate;
+  tts_pitch_text.innerText = tts_pitch.value = settings.pitch;
 
-    // input event for Rate slider (real-time change)
-    tts_rate.addEventListener("input", (e) => {
-        settings.rate = tts_rate.value;
-        tts_rate_text.innerText = tts_rate.value;
-        console.log("TTS Rate: " + tts_rate.value);
-    });
+  // change event for Identifiers list
+  tts_identifiers.addEventListener("change", (e) => {
+    settings.identifier = tts_identifiers.value;
+    console.log("TTS Identifier: " + tts_identifiers.value);
+    setSettings();
+  });
 
-    // change event for Rate slide (Regis final answer)
-    tts_rate.addEventListener("change", (e) => {
-        // it's better to save here once the user has finished dragging
-        setSettings();
-    });
+  // input event for Rate slider (real-time change)
+  tts_rate.addEventListener("input", (e) => {
+    settings.rate = tts_rate.value;
+    tts_rate_text.innerText = tts_rate.value;
+    console.log("TTS Rate: " + tts_rate.value);
+  });
 
-    // input event for Pitch slider (real-time change)
-    tts_pitch.addEventListener("input", (e) => {
-        settings.pitch = tts_pitch.value;
-        tts_pitch_text.innerText = tts_pitch.value;
-        console.log("TTS Pitch: " + tts_pitch.value);
-    });
+  // change event for Rate slide (Regis final answer)
+  tts_rate.addEventListener("change", (e) => {
+    // it's better to save here once the user has finished dragging
+    setSettings();
+  });
 
-    // change event for Pitch slide (Regis final answer)
-    tts_pitch.addEventListener("change", (e) => {
-        // it's better to save here once the user has finished dragging
-        setSettings();
-    });
+  // input event for Pitch slider (real-time change)
+  tts_pitch.addEventListener("input", (e) => {
+    settings.pitch = tts_pitch.value;
+    tts_pitch_text.innerText = tts_pitch.value;
+    console.log("TTS Pitch: " + tts_pitch.value);
+  });
 
-    // click event for the "Say It" button
-    sayit.addEventListener("click", (e) => {
-        var saywhat = document.getElementById("saywhat");
-        var whatissaid = document.getElementById("whatissaid");
-        // make sure you saniatize your input, unlike me
-        var saywhat_str = saywhat.value.trim();
-        // process the input if there is some
-        if (saywhat_str.length > 0) {
-            // start the Potato Talker
-            sup1.play();
-            whatissaid.innerHTML = "";
-            sayit.disabled = true;
-            var idx = 0;
-            // clear any existing timeouts for removing potato jabber
-            if (timeouts.length > 0) {
-                for (var i = 0; i < timeouts.length; i++) {
-                    clearTimeout(timeouts[i]);
-                }
-                timeouts.length = 0;
-            }
-            // use TTS plugin for voice
-            //   TODO: there is different plugin I may use instead
-            //   since it has events for when speech starts and ends
-            //   controlling the animation start/stop would be better
-            TTS.speak({
-                text: saywhat_str,
-                identifier: tts_identifiers.value,
-                rate: tts_rate.value,
-                pitch: tts_pitch.value,
-                cancel: true
-            },
-            function() {
-                // success
-            },
-            function(err) {
-                // error
-            });
-            // Potato Talker speaks character by character
-            var t1 = setInterval(() => {
-                whatissaid.innerHTML += saywhat_str.charAt(idx);
-                if (++idx == saywhat_str.length) { 
-                    clearInterval(t1);
-                    sup1.pause();
-                    sup1.move_to(0);
-                    sayit.disabled = false;
-                    // remove the potato jabber after 3 seconds
-                    timeouts.push(
-                        setTimeout(() => { 
-                            whatissaid.innerHTML = "";
-                        }, 3000)
-                    );
-                }
-            }, 50);
-        } else {
-            // it'd be better if the potato said this
-            alert("Type something to say!");
+  // change event for Pitch slide (Regis final answer)
+  tts_pitch.addEventListener("change", (e) => {
+    // it's better to save here once the user has finished dragging
+    setSettings();
+  });
+
+  // animate the potato gif and button changes
+  function talk(isTalking = true) {
+    if (isTalking) {
+      sup1.play();
+    } else {
+      sup1.pause();
+      sup1.move_to(0);
+      repeatthis.disabled = false;
+    }
+    // reset button regardless
+    repeatthis.innerHTML = "Repeat This";
+    repeatthis.className = "btn btn-primary";
+  }
+
+  // click event for the "Repeat This" button
+  repeatthis.addEventListener("click", (e) => {
+    // use the settings from the tts controls
+    const speechOptions = {
+      speechRate: tts_rate.value,
+      pitchRate: tts_pitch.value,
+      language: tts_identifiers.value,
+    };
+
+    // change the button once it's clicked
+    repeatthis.disabled = true;
+    repeatthis.innerHTML = "I'm Listening...";
+    repeatthis.className = "btn btn-success";
+
+    // start the speech recognition
+    Speech.startRecognition(
+      function (success) {
+        console.log("Speech.startRecognition Success", JSON.stringify(success));
+        // take the speech input's text and print it
+        var idx = 0;
+        whatissaid.innerHTML = "";
+        // clear any existing timeouts for removing potato jabber
+        if (timeouts.length > 0) {
+          for (var i = 0; i < timeouts.length; i++) {
+            clearTimeout(timeouts[i]);
+          }
+          timeouts.length = 0;
         }
-    });
+        // Potato Talker speaks character by character
+        var t1 = setInterval(() => {
+          whatissaid.innerHTML += success.text.charAt(idx);
+          if (++idx == success.text.length) {
+            clearInterval(t1);
+            talk(false);
+            // remove the potato jabber after 3 seconds
+            timeouts.push(
+              setTimeout(() => {
+                whatissaid.innerHTML = "";
+              }, 3000)
+            );
+          }
+        }, 50);
+        // take the speech input and the Potato says it
+        Speech.speakOut(
+          success.text,
+          function (success) {
+            console.log("Speech.speakOut Success", success);
+            talk(success == "tts-start");
+          },
+          function (err) {
+            console.error("Speech.speakOut Error", err);
+            talk(false);
+          },
+          speechOptions
+        );
+      },
+      function (err) {
+        console.error("Speech.startRecognition Error", err);
+        // permission denied if the microphone permission isn't granted
+        //   otherwise, the Potato doesn't understand the speech input
+        const message =
+          err.toLowerCase() != "permission denied"
+            ? "I don't understand gibberish"
+            : "Check your microphone permission";
+        // say the message
+        Speech.speakOut(
+          message,
+          function (success) {
+            console.log("Speech.speakOut Success", success);
+            talk(success == "tts-start");
+          },
+          function (err) {
+            console.error("Speech.speakOut Error", err);
+            talk(false);
+          },
+          speechOptions
+        );
+      }
+    );
+  });
 }
